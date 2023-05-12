f6f84467e9c3adda714400021a265bfa059dc0cc
Bruno Costa <bcosta17.bc@gmail.com>
2023-05-12 16:25:45 -0300

    conclusão do desafio

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -7,8 +7,9 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 	@Input() photoCover: string = '';
-	@Input() cardTitle: string = '';
+	@Input() cardTitle: string = 'UX review presentations ';
 	@Input() cardDescription: string = '';
+	@Input() id: string = '0';
 
 	constructor() {}
 
